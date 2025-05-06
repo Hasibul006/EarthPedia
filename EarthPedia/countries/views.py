6340e32a52d2e255009f55daba931747517fd0fa
Hasibul006 <hi006kuet@gmail.com>
2025-05-06 11:06:38 +0600

    List of all countries

--- EarthPedia/countries/views.py
+++ EarthPedia/countries/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from countries.models import Country
 
 # Create your views here.
+def index(request):
+    countries = Country.objects.all()
+    return render(request, 'index.html', {'countries': countries})
